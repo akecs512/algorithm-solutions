4f8250d7130fefc78f68ece43caf40454066dfab
AK <AK@adriennes-mbp.mynetworksettings.com>
2024-01-04 23:23:08 -0600

    init commit

---
+++ Easy/AddTwoIntegers.js
@@ -0,0 +1,15 @@
+// Given two integers num1 and num2, return the sum of the two integers.
+//Intuition: I need to add the 2 numbers variables together and return the sum
+/**
+ * @param {number} num1
+ * @param {number} num2
+ * @return {number}
+ */
+var sum = function (num1, num2) {
+    //added the number variables together and returned them
+    return (num1 + num2)
+
+};
+
+console.log(sum (12, 5)); //17
+console.log(sum(-10, 4)); //-6
